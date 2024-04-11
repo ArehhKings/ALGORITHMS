54001c704fbb69e888039d36683b7136a400face
Arinzechukwu Kingsley <arehh.kings@gmail.com>
2024-04-11 11:48:17 +0100

    update

---
+++ num/factorial.js
@@ -0,0 +1,37 @@
+// function to calculate factorial of a number
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+//
+
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+
+// function factorial(num) {
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//     if (num > 1) {
+//       return num * factorial(num - 1);
+//     }
+//   }
+// }
+
+function factorial(num) {
+  return num === 0 || num === 1
+    ? 1
+    : num > 1
+    ? num * factorial(num - 1)
+    : "The number is not a positive integer.";
+}
+
+console.log(factorial(-5));
